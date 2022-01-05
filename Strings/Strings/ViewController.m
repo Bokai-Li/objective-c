e9f21a21bb56a5ace39d846ea6f0e3d208cf909d
Bokai Li <bokai247@gmail.com>
2022-01-05 13:27:17 -0500

    add folders without .git

---
+++ Strings/Strings/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  Strings
+//
+//  Created by Aaron Caines on 10/10/2017.
+//  Copyright © 2017 Aaron Caines. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    name = @"Aaron";
+    
+    NSString *word = @"Goodbye";
+    
+    self.label.text = [NSString stringWithFormat:@"%@ %@",word, name];
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
