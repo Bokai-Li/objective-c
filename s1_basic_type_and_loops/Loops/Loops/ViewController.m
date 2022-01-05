7d28c9342e67e97c5ad9d540e86fc8ea4700bf44
Bokai Li <bokai247@gmail.com>
2022-01-05 16:27:28 -0500

    regroup

---
+++ s1_basic_type_and_loops/Loops/Loops/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  Loops
+//
+//  Created by Aaron Caines on 10/10/2017.
+//  Copyright © 2017 Aaron Caines. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    int number = 10;
+    
+    for (number = 20; number > 0; number -= 1) {
+        
+        NSLog(@"%i", number);
+        
+    }
+    
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
