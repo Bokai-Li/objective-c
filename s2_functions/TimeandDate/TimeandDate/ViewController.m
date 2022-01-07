c263d77ad059a35ee10f4402bf6d49893146fd7e
Bokai Li <bokai247@gmail.com>
2022-01-07 00:09:45 -0500

    finishh functions

---
+++ s2_functions/TimeandDate/TimeandDate/ViewController.m
@@ -0,0 +1,49 @@
+//
+//  ViewController.m
+//  TimeandDate
+//
+//  Created by Aaron on 16/06/2017.
+//  Copyright © 2017 Aaron. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self updateTimer];
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateTimer) userInfo:nil repeats:YES];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)updateTimer {
+    
+    NSDateFormatter *timeFormat = [[NSDateFormatter alloc] init];
+    [timeFormat setDateFormat:@"hh:mm:ss"];
+    self.timeLabel.text = [timeFormat stringFromDate:[NSDate date]];
+    
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"dd-MM-YYYY"];
+    self.dateLabel.text = [dateFormat stringFromDate:[NSDate date]];
+    
+    
+    
+}
+
+
+
+
+@end
